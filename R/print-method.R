faad6561e1e2eddbb940b02fb7faf7680e38c3da
Justin Marciszewski <justin@harborislandanalytics.com>
2019-10-28 23:40:45 -0400

    removed likely redundant function cleaned up printing methods to be more easily traced to documentation

--- R/print-method.R
+++ R/print-method.R
@@ -1,20 +1,20 @@
 #' @export
 print.gcat_dataset <- function(x,...){
   cat("==Google Cloud AutoML Table Dataset==\n")
-  cat0("Display Name:               ", x$displayName)
-  cat0("Example Count:              ", x$exampleCount)
-  cat0("Create Time:                ", x$createTime)
-  cat0("Primary Table Spec ID:      ", x[["tablesDatasetMetadata"]][["primaryTableSpecId"]])
+  cat0("displayName:               ", x$displayName)
+  cat0("exampleCount:              ", x$exampleCount)
+  cat0("createTime:                ", x$createTime)
+  cat0("primaryTableSpecId:        ", x[["tablesDatasetMetadata"]][["primaryTableSpecId"]])
 
 }
 
 #' @export
 print.gcat_table_specs <- function(x,...){
-  cat("==Google Cloud AutoML Table==\n")
-  cat0("Rows:                      ", x$rowCount)
-  cat0("Valid Rows:                ", x$validRowCount)
-  cat0("Columns:                   ", x$columnCount)
-  cat0("Name:                      ", x$name)
+  cat("==Google Cloud AutoML TableSpec==\n")
+  cat0("rowCount:                  ", x$rowCount)
+  cat0("validRowCount:             ", x$validRowCount)
+  cat0("columnCount:               ", x$columnCount)
+  cat0("name:                      ", x$name)
   # cat0("Display Name:              ", x$dataset_display_name)# TODO - @justinjm - add display name
 
 }
