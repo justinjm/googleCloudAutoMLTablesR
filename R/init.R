9c2725231c6314458d87ecb8679ad3fd6cd790ee
Justin Marciszewski <justin@harborislandanalytics.com>
2020-03-25 10:42:04 -0400

    added function for setting compute region or location to eliminate need to repeat in other functions #18

--- R/init.R
+++ R/init.R
@@ -1,4 +1,4 @@
-#' Set the location
+#' Set the region
 #'
 #' Can also use environment argument CR_REGION
 #'
@@ -13,5 +13,24 @@ gcat_region_set <- function(region = c("us-central1",
   .gcat_env$region <- region
 
   myMessage("Region set to ", .gcat_env$region, level = 3)
+  .gcat_env$region
+}
+
+#' Get Region Set
+#'
+#' @export
+gcat_region_get <- function(){
+
+  if(!is.null(.gcat_env$region)){
+    return(.gcat_env$region)
+  }
+
+  if(Sys.getenv("GCAT_DEFAULT_REGION") != ""){
+    .gcat_env$region <- Sys.getenv("GCAT_DEFAULT_REGION")
+  }
+  if(is.null(.gcat_env$region)){
+    stop("No region set - use gcat_region_set() or env arg GCAT_DEFAULT_REGION",
+         call. = FALSE)
+  }
   .gcat_env$region
 }
