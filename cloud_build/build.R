0d60bed750a7e4995b9be7df1d7143bad818e178
Justin Marciszewski <justin@harborislandanalytics.com>
2021-04-16 15:03:20 -0400

    Setup automated deployment of package website with googleCloudRunner #22
    * creating test custom build trigger for authenticated vingettes via service account (in secrets manager)
    
    result: unsuccessful

--- cloud_build/build.R
+++ cloud_build/build.R
@@ -1,7 +1,38 @@
 library(googleCloudRunner)
 
+## Create template build trigger ---------------------------------------------
 cr_deploy_pkgdown(
   "justinjm/googleCloudAutoMLTablesR",
   secret = "github-ssh-justinjm",
   create_trigger = "inline"
 )
+
+
+## Create custom build steps -----------------------------------------------------
+yaml <- cr_build_yaml(
+  steps = c(
+    cr_buildstep_secret(
+      id = "download service account json for authenticated vingettes",
+      "gc-automl-tables-r-service-account",
+      decrypted = "/workspace/gc-automl-tables-r-291b441d9ddd.json"
+    ),
+
+    cr_buildstep_pkgdown(
+      github_repo = "justinjm/googleCloudAutoMLTablesR",
+      git_email = "justin@harborislandanalytics.com",
+      secret = "github-ssh-justinjm",
+      env = c("GAR_SERVICE_JSON=/workspace/gc-automl-tables-r-291b441d9ddd.json",
+              "GCAT_DEFAULT_PROJECT_ID=gc-automl-tables-r")
+    )
+  )
+)
+
+print(yaml)
+
+# https://code.markedmondson.me/googleCloudRunner/articles/cloudbuild.html#build-triggers-via-code-1
+
+bb <- cr_build_make(yaml = yaml)
+
+cr_buildtrigger_repo("justinjm/googleCloudAutoMLTablesR", branch = "master")
+
+cr_buildtrigger(bb, name = "test-pkgdown1", trigger = github)
